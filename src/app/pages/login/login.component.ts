9af9e2c1041b24a717522ac7577795209dbd8cb5
Guadalupe Lopez <gvlrinfo2023@gmail.com>
2026-07-20 08:15:04 -0600

    Correciòn de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  usuario = '';
+  password = '';
+
   constructor(private router: Router) {}
 
   login() {
-    this.router.navigate(['/clientes']);
+    if (this.usuario === 'admin' && this.password === '12345') {
+      localStorage.setItem('logueado', 'true');
+      this.router.navigate(['/clientes']);
+    } else {
+      alert('Usuario o contraseña incorrectos');
+    }
   }
 
 }
